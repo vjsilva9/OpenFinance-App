b6d05dd04dfcddcb0c56880f9ea50701980e0e6f
VJ Silva <macos@192.168.1.40>
2026-07-18 14:54:39 -0300

    Adicionado sistema de recebimento e pagamento

--- Open_Financia/script.js
+++ Open_Financia/script.js
@@ -4,34 +4,58 @@ const temaWhite = document.getElementById("temaWhite");
 const temaBlack = document.getElementById("temaBlack");
 
 temaBlack.addEventListener("click", () => {
-
     document.body.classList.add("dark");
-
 });
 
 temaWhite.addEventListener("click", () => {
-
     document.body.classList.remove("dark");
-
 });
-console.log(temaWhite);
-console.log(temaBlack);
 
-const saldo = 24990;
-const receita = 10990;
-const despesas = 4490;
-const investimento = 16490;
+let saldo = 24990;
+let receita = 10990;
+let despesas = 4490;
+let investimento = 16490;
 
-document.getElementById("saldoTotal").innerText = 
-`R$ ${saldo.toLocaleString("pt-BR")}`;
+const btnReceber = document.getElementById("btnReceber");
+const btnDespesa = document.getElementById("btnDespesa");
 
-document.getElementById("receita").innerText = 
-`R$ ${receita.toLocaleString("pt-BR")}`;
+btnReceber.addEventListener("click", () => {
+    receberDinheiro(1000);
+});
 
-document.getElementById("despesas").innerText = 
-`R$ ${despesas.toLocaleString("pt-BR")}`;
+btnDespesa.addEventListener("click", () => {
+    pagarConta(500);
+});
 
-document.getElementById("investimento").innerText = 
-`R$ ${investimento.toLocaleString("pt-BR")}`;
+function receberDinheiro(valor) {
 
-saldo.innerHTML = "R$ 30.000,00";
+    saldo += valor;
+    receita += valor;
+
+    atualizarDashboard();
+}
+
+function pagarConta(valor) {
+
+    saldo -= valor;
+    despesas += valor;
+
+    atualizarDashboard();
+}
+
+atualizarDashboard();
+
+function atualizarDashboard() {
+
+    document.getElementById("saldoTotal").innerText =
+        `R$ ${saldo.toLocaleString("pt-BR")}`;
+
+    document.getElementById("receita").innerText =
+        `R$ ${receita.toLocaleString("pt-BR")}`;
+
+    document.getElementById("despesas").innerText =
+        `R$ ${despesas.toLocaleString("pt-BR")}`;
+
+    document.getElementById("investimento").innerText =
+        `R$ ${investimento.toLocaleString("pt-BR")}`;
+}
